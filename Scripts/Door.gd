921b3ba392d438e7587cdba83eedb7ee75736f22
Doubi88 <tobis_mail@yahoo.de>
2020-07-20 23:29:48 +0200

    Implemented time travel on the door

--- Scripts/Door.gd
+++ Scripts/Door.gd
@@ -1,17 +1,29 @@
-extends Area2D
+extends TimedObject
 
 class_name Door
 
 export var isOpen = false
+export var timer: Resource
 
 func _process(delta: float) -> void:
 	if (isOpen):
 		$AnimatedSprite.frame = 1
 	else:
 		$AnimatedSprite.frame = 0
+		
+	if (timer.reverse):
+		var action = getAction(round(timer.timeLeft))
+		if (action == "open"):
+			isOpen = false
+		elif (action == "close"):
+			isOpen = true
 
 func use():
 	isOpen = !isOpen
+	if (isOpen):
+		saveAction(round(timer.timeLeft), "open")
+	else:
+		saveAction(round(timer.timeLeft), "close")
 
 
 func _on_Area_body_entered(body: Node) -> void:
--- Scripts/TimedObject.gd
+++ Scripts/TimedObject.gd
@@ -2,3 +2,15 @@ extends Node2D
 
 class_name TimedObject
 
+var actions = {}
+
+func saveAction(second: int, action):
+	actions[second] = action
+
+func getAction(second: int):
+	if (actions.has(second)):
+		var action = actions[second]
+		actions.erase(second)
+		return action
+	else:
+		return null
